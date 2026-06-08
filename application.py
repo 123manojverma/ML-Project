f13b486dc9def8d3058d4896c44b0ac63dfa488c
Manoj Verma <123manojverma2005@gmail.com>
2026-06-08 21:24:51 +0530

    Deployment Configuration for AWS

--- application.py
+++ application.py
@@ -37,4 +37,7 @@ def predict_datapoint():
         return render_template('home.html',results=results[0])
     
 if __name__=="__main__":
+    # while deployment remove debug=True
+    # app.run(host="0.0.0.0")
     app.run(host="0.0.0.0",debug=True)
+    
